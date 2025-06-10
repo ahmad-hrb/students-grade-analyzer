859eaa2df6e40f9c88c79ba3ce326642ba2117c2
AhmadHarb <hareb9020@gmail.com>
2025-06-10 12:31:26 +0300

    Add New Function inside the first Function  'get_heighest_grade' to get heighest grade

--- grade-analyzer.py
+++ grade-analyzer.py
@@ -20,3 +20,19 @@ def display_student_summary():
         print("Te Average Of Grades Is :",avg )
         return avg 
     get_avg_grade()
+    
+      # function to get the highest grade
+    def get_heighest_grade():
+        greater_grd = list_grade[0]
+        num = 0
+        for i , ele in enumerate(list_grade):
+            
+            if ele > greater_grd :
+                greater_grd = ele
+                num = i
+        print("the highest grade is :" , greater_grd)
+        print("the name of the student have highest grade is :" , list_name[num])
+    get_heighest_grade()
+display_student_summary()
+
+    
